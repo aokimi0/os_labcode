6301c86d334b52fa163fc48bf9252a1a5e493656
ghost233lism <jin_modi@outlook.com>
2025-10-18 04:12:55 +0800

    fix

--- lab2/tools/grade_slub.sh
+++ lab2/tools/grade_slub.sh
@@ -6,27 +6,24 @@ if [ "x$1" = "x-v" ]; then
     out=/dev/stdout
     err=/dev/stderr
 else
-    out=/dev/stdout
-    err=/dev/stderr
+    out=/dev/null
+    err=/dev/null
 fi
 
 if gmake --version > /dev/null 2>&1; then make=gmake; else make=make; fi
 makeopts="--quiet --no-print-directory -j"
 
-make_print() { echo `$make $makeopts print-$1`; }
-
 awk='awk'; bc='bc'; date='date'; grep='grep'; rm='rm -f'; sed='sed'
 sym_table='obj/kernel.sym'
-gdb="$(make_print GDB)"; gdbport='1234'
-gdb_in="$(make_print GRADE_GDB_IN)"
-qemu="$(make_print qemu)"
-qemu_out="$(make_print GRADE_QEMU_OUT)"
+gdb='riscv64-unknown-elf-gdb'; gdbport='1234'
+gdb_in='.gdb.in'
+qemu='qemu-system-riscv64'
+qemu_out='.qemu.out'
 
 if $qemu -nographic -help | grep -q '^-gdb'; then qemugdb="-gdb tcp::$gdbport"; else qemugdb="-s -p $gdbport"; fi
 default_timeout=30; pts=5; part=0; part_pos=0; total=0; total_pos=0
 
 update_score(){ total=`expr $total + $part`; total_pos=`expr $total_pos + $part_pos`; part=0; part_pos=0; }
-get_time(){ echo `$date +%s.%N 2> /dev/null`; }
 show_build_tag(){ echo "$1:" | $awk '{printf "%-24s ", $0}'; }
 show_check_tag(){ echo "$1:" | $awk '{printf "  -%-40s  ", $0}'; }
 show_msg(){ echo $1; shift; if [ $# -gt 0 ]; then echo -e "$@" | awk '{printf "   %s\n", $0}'; echo; fi }
@@ -37,7 +34,6 @@ run_qemu(){
     qemuextra=
     if [ -n "$brkfun" ]; then qemuextra="-S $qemugdb"; fi
     if [ -z "$timeout" ] || [ $timeout -le 0 ]; then timeout=$default_timeout; fi
-    t0=$(get_time)
     (
         ulimit -t $timeout
         exec $qemu -nographic $qemuopts -serial file:$qemu_out -monitor null -no-reboot $qemuextra
@@ -73,7 +69,6 @@ check_regexps(){
 }
 
 run_test(){ tag=; check=check_regexps; while true; do select=; case $1 in -tag) select=`expr substr $1 2 ${#1}`; eval $select='$2';; esac; if [ -z "$select" ]; then break; fi; shift; shift; done; defs=; while expr "x$1" : "x-D.*" > /dev/null; do defs="DEFS+='$1' $defs"; shift; done; if [ "x$1" = "x-check" ]; then check=$2; shift; shift; fi; $make $makeopts touch > /dev/null 2>&1; build_run "$tag" "$defs"; check_result 'check result' "$check" "$@"; }
-quick_check(){ tag="$1"; shift; show_check_tag "$tag"; check_regexps "$@"; }
 
 osimg=$(make_print ucoreimg)
 qemuopts="-machine virt -nographic -bios default -device loader,file=bin/ucore.img,addr=0x80200000"
